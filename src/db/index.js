85390b226bec0d51d37e2734e09489a77a117d2a
David Ajnered <david@ajnered.com>
2018-09-26 21:09:40 +0200

    Using same .env variables as Laravel

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mysql from 'mysql';
 
 var connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
