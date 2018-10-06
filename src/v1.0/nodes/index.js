a8bf92a3caa75e2a87b3e77cbb996c3d4eb24c41
David Ajnered <david@ajnered.com>
2018-10-06 16:13:04 +0200

    Node names

--- src/v1.0/nodes/index.js
+++ src/v1.0/nodes/index.js
@@ -4,6 +4,46 @@ import nodes from './model';
 var router = express.Router();
 
 /**
+ * @api {get} /nodes/names Get nodes names
+ * @apiGroup Nodes
+ * @apiVersion 1.0.0
+ * @apiDescription Get the names for nodes on localfoodnodes.org
+ *
+ * @apiSuccess {Object} data Name of nodes.
+ *
+ * @apiSuccessExample Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *   "data": {
+ *     3: "Bygdens Saluhall Röstånga",
+ *     4: "Bygdens Saluhall Dalby",
+ *     8: "Mejeriet Food Node - Österfärnebo",
+ *     10: "Södertörns Reko/matnod",
+ *     ...
+ *   }
+ * }
+ *
+ * @apiError (Error 500) {Object} error Object containing error message.
+ *
+ * @apiErrorExample Error-Response:
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "error": {
+ *     message: "A message describing the error."
+ *   }
+ * }
+ */
+router.get('/names', (req, res) => {
+  nodes.names()
+  .then(data => {
+    res.send(data);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  });
+});
+
+/**
  * @api {get} /nodes/count Get nodes count
  * @apiGroup Nodes
  * @apiVersion 1.0.0
--- src/v1.0/nodes/model.js
+++ src/v1.0/nodes/model.js
@@ -7,4 +7,23 @@ export default {
   count() {
     return db.query('SELECT data FROM statistics WHERE statistics.key = ?', ['node_count']);
   },
+
+  /**
+   * Get node names
+   */
+  names() {
+    return db.query('SELECT id, name FROM nodes')
+    .then(results => {
+      let data = {};
+
+      for (let nodeId in results.data) {
+        let node = results.data[nodeId];
+        data[node.id] = node.name;
+      }
+
+      return {
+        data: data
+      }
+    });
+  },
 }
